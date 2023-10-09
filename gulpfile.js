8f8a59c270547ff77685caf111b511109cbbf24f
anusha-c18 <anushac2001@gmail.com>
2023-10-09 15:27:56 +0530

    feat: purging unused css

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,17 @@
 const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const purgecss = require("gulp-purgecss");
 
 function buildStyles() {
-  return src("./jelly/**/*.scss").pipe(sass()).pipe(dest("css"));
+  return src("./jelly/**/*.scss")
+    .pipe(sass())
+    .pipe(purgecss({ content: ["*.html"] }))
+    .pipe(dest("css"));
   //compiles the sass file and outputs the compiled css files into the css folder
 }
 
 function watchTask() {
-  watch(["./jelly/**/*.scss"], buildStyles); //calls the function buildStyles whenever there are changes in the dependencies listed in the array
+  watch(["./jelly/**/*.scss", "*.html"], buildStyles); //calls the function buildStyles whenever there are changes in the dependencies listed in the array
 }
 
 exports.default = series(buildStyles, watchTask);

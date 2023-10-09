a43b44c62c4946a62395ab666ed022c838da835c
anusha-c18 <anushac2001@gmail.com>
2023-10-09 12:46:14 +0530

    init

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+
+function buildStyles() {
+  return src("index.scss").pipe(sass()).pipe(dest("css"));
+  //compiles the sass file and outputs the compiled css files into the css folder
+}
+
+function watchTask() {
+  watch(["index.scss"], buildStyles); //calls the function buildStyles whenever there are changes in the dependencies listed in the array
+}
+
+exports.default = series(buildStyles, watchTask);

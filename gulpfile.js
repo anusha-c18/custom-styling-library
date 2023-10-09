3e530a6abb7911b30df172c4a457fe09a5307612
anusha-c18 <anushac2001@gmail.com>
2023-10-09 13:16:34 +0530

    feat: card styles

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,12 @@ const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 
 function buildStyles() {
-  return src("index.scss").pipe(sass()).pipe(dest("css"));
+  return src("./jelly/**/*.scss").pipe(sass()).pipe(dest("css"));
   //compiles the sass file and outputs the compiled css files into the css folder
 }
 
 function watchTask() {
-  watch(["*.scss"], buildStyles); //calls the function buildStyles whenever there are changes in the dependencies listed in the array
+  watch(["./jelly/**/*.scss"], buildStyles); //calls the function buildStyles whenever there are changes in the dependencies listed in the array
 }
 
 exports.default = series(buildStyles, watchTask);
